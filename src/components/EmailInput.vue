3f0810eeaec28e4300ac743241b55d5677b36eb5
abumaybah <abdulazizmahmudov01@gmail.com>
2022-09-09 20:12:02 +0500

    reactivety error

---
+++ src/components/EmailInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <input
+        type="email"
+        class="form-control"
+        placeholder="E-mail"
+        v-model="value"
+        :class="{
+            'is-valid': value.length >= 4 && value.length > 0,
+            'is-invalid': value.length < 4 && value.length > 0,
+        }"
+    />
+</template>
+<script>
+export default {
+    name: 'EmailInput',
+    data() {
+        return {
+            value: '',
+        };
+    },
+};
+</script>
